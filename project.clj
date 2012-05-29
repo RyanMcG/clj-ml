e487b1ab0b5a874176fd536f26ea28e2d1ca2e07
Ryan McGowan <ryan@ryanmcg.com>
2012-05-29 18:10:42 -0400

    Removes dev dependencies and changes repository.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject com.leadtune/clj-ml "0.2.2"
+(defproject org.clojars.ryanmcg/clj-ml "0.2.2-SNAPSHOT"
   :description "Machine Learning library for Clojure built around Weka and friends"
-  :repositories {"leadtune-repo" "http://utility.leadtune.com:8081/nexus/content/repositories/thirdparty/"}
+  :repositories {"leadtune-repo" "http://c0026236.cdn1.cloudfiles.rackspacecloud.com/repo"}
   :aoc "src/java"
   :javac-fork "true"
   :warn-on-reflection true
@@ -8,6 +8,4 @@
                  [incanter/incanter-core "1.3.0-SNAPSHOT"]
                  [incanter/incanter-charts "1.3.0-SNAPSHOT"]
                  [lt/weka "3.6.3"]
-                 [hr.irb/fastRandomForest "0.98"]]
-  :dev-dependencies [[midje "1.3.0-RC2"]
-                     [swank-clojure "1.3.4"]])
+                 [hr.irb/fastRandomForest "0.98"]])
