6506c93f0db934387c306452da83bc1e21a6264d
Ryan McGowan <ryan@ryanmcg.com>
2012-05-29 20:53:39 -0400

    Decrement version and update project.clj to leiningen 2.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject org.clojars.ryanmcg/clj-ml "0.2.2-SNAPSHOT"
+(defproject org.clojars.ryanmcg/clj-ml "0.2.1-SNAPSHOT"
   :description "Machine Learning library for Clojure built around Weka and friends"
   :repositories {"leadtune-repo" "http://c0026236.cdn1.cloudfiles.rackspacecloud.com/repo"}
-  :aoc "src/java"
-  :javac-fork "true"
+  :java-source-paths ["src/java"]
+  :javac-options [:fork "true"]
   :warn-on-reflection true
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [incanter/incanter-core "1.3.0-SNAPSHOT"]
